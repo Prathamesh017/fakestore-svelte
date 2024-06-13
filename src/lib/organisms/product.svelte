f8a17f77cfd2f251f24a0034f805ffb45af37661
prathamesh thakare <prathamesh06pt@gmail.com>
2024-06-13 18:00:19 +0530

    feat:cart implementation completed

--- src/lib/organisms/product.svelte
+++ src/lib/organisms/product.svelte
@@ -2,11 +2,21 @@
 <script  lang="ts">
   import Button from "$lib/atoms/button.svelte";
   import { goto } from '$app/navigation';
+  import { getContext } from 'svelte';
 	import type { product } from "../../common/type";
+  const cartStore:any= getContext('cart');
   export let product:product;
+
+  function addToCart(title:string,price:number){
+    cartStore.update((cart:any)=>{
+      cart.push({title,price});
+      return cart;
+    })
+    goto("/cart")
+  }
 </script>
-<div class="product w-4/5 border grid grid-cols-2 bg-[#2f4558] rounded-lg">
-<div class="product-image-container p-4 w-1/2">
+<div class="product w-4/5 border  grid  lg:grid-cols-2 bg-[#2f4558] rounded-lg">
+<div class="product-image-container p-4 w-full lg:w-1/2">
   <img class=" mix-blend-multiply" src={product.image} alt="id">
 </div>
   <div class="flex flex-col product-details p-4">
@@ -22,7 +32,7 @@
     </div>
     <div class="buttons-container flex w-full">
       <Button classes="w-1/2 border p-2 bg-[#2E63D7ff] hover:text-white " label={"Back"} onClick={()=>{goto("/")}}></Button>
-      <Button classes="w-1/2 p-2 border bg-[#06D001] hover:text-white" label={"Add to Cart"} onClick={()=>{goto("/cart")}}></Button>
+      <Button classes="w-1/2 p-2 border bg-[#06D001] hover:text-white" label={"Add to Cart"} onClick={()=>{addToCart(product.title,product.price)}}></Button>
     </div>
   </div>
 </div>
